822f69bac4ce2ba663dcde830683c6d327673c5c
Valmir Garcia <val8558@gmail.com>
2023-11-14 08:38:35 -0300

    teste navbar

--- src/pages/ReportPage/ReportTable.jsx
+++ src/pages/ReportPage/ReportTable.jsx
@@ -1,12 +1,12 @@
 import Header from '../../Components/Header/Header'
 import Cards from '../../Components/Qualifications/QualificationsCards'
 import PDFGeneratorButton from '../../Components/PdfButton/PdfButton'
-import NavBar from '@/Components/navBar/navBar'
 import Table from '../../Components/Tables/Table/Table'
 import TableCompare from '../../Components/Tables/TableCompare/TableCompare'
 import Footer from '../../Components/Footer/Footer'
 import TableExemple from '../../Components/Tables/TableExemple/TableExemple'
 import TableResult from '../../Components/Tables/TableResult/TableResult'
+import NavBar from '../../Components/navBar'
 
 
 function ReportTable() {
@@ -15,8 +15,8 @@ function ReportTable() {
     <Header />
     <PDFGeneratorButton />
     <Cards />
-    <NavBar />
     <TableExemple />
+    <NavBar />
     <Table />
     <TableResult />
     <TableCompare />
