ae7aa9e87ca06d8927d9e59536bc973b41adb520
Valmir <111082887+val8558@users.noreply.github.com>
2024-02-02 16:04:41 -0300

    Retorno da identificação do Paciente

--- src/Components/Qualifications/QualificationsCards.jsx
+++ src/Components/Qualifications/QualificationsCards.jsx
@@ -6,7 +6,8 @@ function Cards() {
 
     const {apiData} = useAuth()
 
-  const doctorData = apiData.doctor
+  const doctorData = apiData.doctor;
+  const patientData = apiData.patient
 
 
     return (
@@ -17,11 +18,24 @@ function Cards() {
                     <br />
                     <p><strong>Nome:</strong> {doctorData.nome}</p>
                     <p><strong>CRM:</strong> {doctorData.register}</p>
-                    <p><strong>Área de atuaçâo:</strong> {doctorData.occuoation_area}</p>
+                    <p><strong>Área de atuaçâo:</strong> {doctorData.occuopation_area}</p>
                     <p><strong>Telefone:</strong> {doctorData.number}</p>
                     <p><strong>E-mail:</strong> {doctorData.number}</p>
                 </div>
+
+                <div className={style.patient}>
+                    <h1>DADOS GERAIS DO PACIENTE</h1>
+                    <br />
+                    <p><strong>Nome:</strong> {patientData.name}</p>
+                    <p><strong>Profissão:</strong> {patientData.work}</p>
+                    <p><strong>Lateralidade:</strong> {patientData.laterality}</p>
+                    <p><strong>Escolaridade:</strong> {patientData.category}</p>
+                    <p><strong>E-mail:</strong> {patientData.email}</p>
+                </div>
             </div>
+
+
+          
         </>
     )
 }
